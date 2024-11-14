7b0648bb5959447e39b71e25db76a723b7ee66cd
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-14 15:47:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-// function CreatePost(){
-//     var input = document.getElementById("inputtext");
-//     var main = document.getElementsByClassName("main")
-     
-//     main.innerHTML = input.value;
-//     input.value = ""
-// }
 
-// var main = document.getElementsByTagName("h1");
-// console.log(main[0]);
-// var main = document.getElementById("main")
-// var heading = main.getElementsByTagName("h1")
-// console.log(heading[0].innerHTML);
 
 
 function CreatePost() {
