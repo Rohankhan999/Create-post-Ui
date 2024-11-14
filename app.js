e3c25b7129480ab1d7ce6b3427188ce60f685eae
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-14 15:45:12 +0500

    Done

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-function CreatePost(){
-    var input = document.getElementById("inputtext");
-    var main = document.getElementsByClassName("Posts")
+// function CreatePost(){
+//     var input = document.getElementById("inputtext");
+//     var main = document.getElementsByClassName("main")
      
-    main.innerHTML = input.value;
-    input.value = ""
-}
-var main = document.getElementById("main");
-console.log(main[0])
-var main = document.getElementsBy()
-var heading = main.document.getElementById("")
-console.log(heading.innerHTML);
+//     main.innerHTML = input.value;
+//     input.value = ""
+// }
 
+// var main = document.getElementsByTagName("h1");
+// console.log(main[0]);
+// var main = document.getElementById("main")
+// var heading = main.getElementsByTagName("h1")
+// console.log(heading[0].innerHTML);
+
+
+function CreatePost() {
+    var input = document.getElementById("inputtext");
+    var mainHeading = document.getElementById("mainHeading");
+    
+    mainHeading.innerHTML = input.value;
+    input.value = ""; 
+}
+
+var mainContent = document.getElementById("mainContent");
+var heading = mainContent.getElementsByTagName("h1");
+console.log(heading[0].innerHTML); 
 
 
