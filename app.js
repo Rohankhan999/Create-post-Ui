625a0f6bbc33f80bfbb6e1214a0828057793e291
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-14 22:01:41 +0500

    file

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+// function CreatePost() {
+//     var input = document.getElementById("inputtext");
+//     var mainHeading = document.getElementById("mainHeading");
+    
+//     mainHeading.innerHTML = input.value;
+//     input.value = ""; 
+// }
+
+// var mainContent = document.getElementById("mainContent");
+// var heading = mainContent.getElementsByTagName("h1");
+// console.log(heading[0].innerHTML); 
+
+
 function CreatePost() {
     var input = document.getElementById("inputtext");
-    var mainHeading = document.getElementById("mainHeading");
-    
-    mainHeading.innerHTML = input.value;
-    input.value = ""; 
+    var postContainer = document.getElementById("postContainer");
+
+    if (input.value.trim() !== "") {
+        // Create a new div for the post content
+        var newPost = document.createElement("div");
+        newPost.className = "singlePost";
+        newPost.innerHTML = `<h2>${input.value}</h2>`;
+
+        // Append the new post to the post container
+        postContainer.appendChild(newPost);
+
+        // Clear the input field
+        input.value = "";
+    } else {
+        alert("Please enter some text to post.");
+    }
 }
-
-var mainContent = document.getElementById("mainContent");
-var heading = mainContent.getElementsByTagName("h1");
-console.log(heading[0].innerHTML); 
-
-
